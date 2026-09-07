01f88108821241ecfc64d8be2dd833c254491403
JsurDev <juansurianodev@gmail.com>
2026-09-06 21:33:06 -0600

    inicial

---
+++ callback.js
@@ -0,0 +1,18 @@
+//callback = una funcion que se pasa como argumento a otra funcion
+
+programando(descansando);
+programando(debugueando);
+
+
+function programando(callback){    
+    callback();
+    console.log("The best copycat");
+}
+
+function debugueando(){
+    console.log("Buscando errores");
+}
+
+function descansando() {
+  console.log("Tomando un café...");
+}
